9b8ea1d24bc3a7320c6ca2eebacb1dc2f070eb50
Florian Gamböck <mail@floga.de>
2022-03-17 23:29:57 +0100

    Search bump config in multiple locations
    The bump config files will be searched in the following locations:
    
    -   $HOME/.git-bump.lua
    
        Per-user global config file.
    
    -   $GIT_DIR/git-bump.lua
    
        Per-repository config file, not intended for sharing.
    
    -   $GIT_WORK_TREE/.git-bump.lua
    
        Per-repository config file, checked into Git for sharing.
    
    Those locations will be evaluated in order, a later file overrides
    mappings of the previous ones if they have matching keys. Missing config
    files will be silently ignored unless all files are missing, which
    results in a panic, since `git bump` needs at least one config file to
    do something.
    
    Also, this commit makes the file mappings relative to the
    $GIT_WORK_TREE, so that `git bump` can be called from anywhere in the
    repository and it will always modify the correct files.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,45 @@ use mlua::prelude::*;
 use mlua::Function;
 
 fn main() -> LuaResult<()> {
+    let repository = git2::Repository::discover(".").expect("Not a Git repository");
+
+    let workdir = match repository.workdir() {
+        Some(workdir) => workdir,
+        None => panic!("git-bump is not supported on bare repositories"),
+    };
+
+    let bump_configs = {
+        let config_user =
+            home::home_dir().and_then(|p| p.join(".git-bump.lua").canonicalize().ok());
+        let config_repo_unshared = repository.path().join("git-bump.lua").canonicalize().ok();
+        let config_repo_shared = workdir.join(".git-bump.lua").canonicalize().ok();
+
+        [config_user, config_repo_unshared, config_repo_shared]
+            .into_iter()
+            .flatten()
+            .collect::<Vec<_>>()
+    };
+
+    if bump_configs.is_empty() {
+        panic!("No valid config files found")
+    }
+
     let version = args().nth(1).unwrap();
 
     let lua = Lua::new();
 
-    let map = lua
-        .load(&fs::read_to_string("bump.lua").unwrap())
-        .eval::<HashMap<String, Function>>()?;
+    let mut map = HashMap::new();
+    for config in bump_configs {
+        let content = fs::read_to_string(config);
+        let chunk = match content {
+            Ok(content) => lua.load(&content).eval::<HashMap<String, Function>>(),
+            Err(_) => continue,
+        };
+
+        for (file, func) in chunk? {
+            map.insert(file, func);
+        }
+    }
 
     for (file, f) in map {
         let mut contents = f.call::<_, String>(version.clone())?;
@@ -20,7 +52,7 @@ fn main() -> LuaResult<()> {
             contents.push('\n')
         }
 
-        fs::write(file, contents).unwrap();
+        fs::write(workdir.join(file), contents).unwrap();
     }
 
     Ok(())
