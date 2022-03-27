53cff951ecdb8297782016c8af73ec79cf7b84e0
Florian Gamböck <mail@floga.de>
2022-03-27 23:59:07 +0200

    Use cli module as main entry point

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,15 @@
 use clap::Parser;
 
+use crate::{bump, Result};
+
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     /// Version to set
     version: String,
+}
+
+pub fn run() -> Result<()> {
+    let cli = Cli::parse();
+    bump(cli.version)
 }
