ae6112810b91ffe653356990f94b7a347041a92b
Florian Gamböck <mail@floga.de>
2022-03-31 17:01:46 +0200

    Prepare for different actions

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,29 @@
-use clap::Parser;
+use clap::{ArgGroup, Parser};
 
 use crate::{bump, Result};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
+#[clap(group(
+    ArgGroup::new("action")
+        .required(true)
+        .args(&[
+            "version",
+        ]),
+))]
 struct Cli {
     /// Version to set
-    version: String,
+    version: Option<String>,
 }
 
 pub fn run() -> Result<()> {
     let cli = Cli::parse();
-    bump(cli.version)
+
+    if let Some(version) = cli.version {
+        bump(version)?
+    }
+
+    Ok(())
 }
 
 #[test]
