356028aac373bc04b45da6a433fff5ec8c6df0fc
Florian Gamböck <mail@floga.de>
2022-04-01 14:23:17 +0200

    Make sample config accessible in cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use clap::{ArgGroup, Parser};
 
-use crate::{bump, Result};
+use crate::{bump, print_sample_config, Result};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -9,11 +9,16 @@ use crate::{bump, Result};
         .required(true)
         .args(&[
             "version",
+            "print-sample-config",
         ]),
 ))]
 struct Cli {
     /// Version to set
     version: Option<String>,
+
+    #[clap(long)]
+    /// Print sample config file
+    print_sample_config: bool,
 }
 
 pub fn run() -> Result<()> {
@@ -21,6 +26,8 @@ pub fn run() -> Result<()> {
 
     if let Some(version) = cli.version {
         bump(version)?
+    } else if cli.print_sample_config {
+        print_sample_config()
     }
 
     Ok(())
